421f034a3e25a1ebd087ddfb1f750aa1413b162c
Christian Tisby <cduane82@hotmail.com>
2020-03-16 22:12:23 -0400

    first commit

---
+++ 04_App_Structure/numpicker.js
@@ -0,0 +1,19 @@
+function getNum() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+class NumPicker extends React.Component {
+    render() {
+        const num = getNum()
+        return (
+            <div>
+              <h1>Your number is {num}</h1>
+              <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
+              
+              {
+                  num === 7 && <img src="https://media.giphy.com/media/uTuLngvL9p0Xe/giphy.gif"/>
+              }
+            </div>
+        )
+     }
+  }
+
